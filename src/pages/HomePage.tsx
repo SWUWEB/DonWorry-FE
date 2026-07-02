5b50d586e434f6f825d5d475bc5828599a96f14b
sample <sample@naver.com>
2026-07-02 23:55:30 +0900

    fix: 테스트 코드 삭제

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,11 @@
 import Header from '@/components/layout/Header'
-import { ProductForm, type FormData } from '@/components/layout/ProductForm'
 import Home from '@/features/home'
 
 export default function HomePage() {
-  const handleSubmit = (data: FormData) => {
-    console.log('제출된 데이터:', data);
-  };
-
   return (
     <>
       <Header />
       <Home />
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        <ProductForm
-          showTimeSelector={true} 
-          onSubmit={handleSubmit} 
-        />
-      </div>
     </>
   )
 }
