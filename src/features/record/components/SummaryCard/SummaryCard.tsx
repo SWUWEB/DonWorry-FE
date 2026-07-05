8763f66447942b822ac3a735342fdeeacd99bcfa
yujining3827 <j_yujin_swu22@naver.com>
2026-07-05 22:20:10 +0900

    ✨ feat: SummaryCard 컴포넌트 구현

---
+++ src/features/record/components/SummaryCard/SummaryCard.tsx
@@ -0,0 +1,48 @@
+import styles from "./SummaryCard.module.css";
+import legendBg from "@/assets/legend-bg.svg";
+
+export default function SummaryCard() {
+  return (
+    <section className={styles.summary}>
+      <div className={styles.card}>
+        <div className={styles.left}>
+          <p className={styles.title}>소비 vs 참은 소비</p>
+
+          <h2 className={styles.price}>+96,500원</h2>
+
+          <div className={styles.legendWrapper}>
+            <img
+              src={legendBg}
+              alt=""
+              className={styles.legendBg}
+            />
+
+            <div className={styles.legend}>
+              <div className={styles.legendItem}>
+                <div className={styles.leftItem}>
+                  <span className={`${styles.dot} ${styles.save}`} />
+                  <span>참았어요</span>
+                </div>
+
+                <span className={styles.percent}>65%</span>
+              </div>
+
+              <div className={styles.legendItem}>
+                <div className={styles.leftItem}>
+                  <span className={`${styles.dot} ${styles.consume}`} />
+                  <span>샀어요</span>
+                </div>
+
+                <span className={styles.percent}>35%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.chart}>
+          Chart
+        </div>
+      </div>
+    </section>
+  );
+}
