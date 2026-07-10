9ccaa74ed01b40b109633ffd1e36cb421aef7866
손예원 <abbySonyo6@gmail.com>
2026-07-10 09:38:56 +0900

    fix: remove unavailable routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import HomePage from '@/pages/HomePage'
 import LoginPage from '@/pages/LoginPage'
-import SignUp from '@/features/auth/SignUp'
-import MyPagePage from '@/pages/MyPagePage'
+//import SignUp from '@/features/auth/SignUp'
+//import MyPagePage from '@/pages/MyPagePage'
 
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
   { path: '/login', element: <LoginPage /> },
-  { path: '/signup', element: <SignUp /> },
-  { path: '/mypage', element: <MyPagePage /> },
+  //{ path: '/signup', element: <SignUp /> },
+  //{ path: '/mypage', element: <MyPagePage /> },
   // TODO: 랜딩, 로그인, 소비기록, 유혹관리, 마이페이지 라우트 추가
 ])
 
